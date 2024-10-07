7ef7ee2bb03014de1a994b4d343403a5fcf30e38
David Moravec <david.moravec2@seznam.cz>
2024-10-07 19:48:03 +0200

    slider 1.1 - pokrocily, automaticky posuvny

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import OneMovieSlider from "./components/OneMovieSlider";
+
+function App() {
+  return (
+    <div className="slider">
+      <OneMovieSlider></OneMovieSlider>
+    </div>
+  );
+}
+
+export default App;
